764c68b41392714dbac96499ad2c3d2e43464a5b
Rerispingo <bernardo.o.horta@gmail.com>
2026-08-10 13:07:44 -0300

    feat: add player wasd movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,75 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private float gravity = -9.81f;
+
+    private CharacterController characterController;
+    private Vector3 verticalVelocity;
+    private Vector2 moveInput;
+    private bool jumpRequested;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HorizontalMovement();
+        VerticalMovement();
     }
+
+    // --------------- Input Actions ---------------
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            jumpRequested = true;
+        }
+    }
+
+    // --------------- Movement ---------------
+
+    private void HorizontalMovement()
+    {
+        Vector3 movementDirection = (transform.right * moveInput.x) + (transform.forward * moveInput.y);
+        movementDirection = Vector3.ClampMagnitude(movementDirection, 1f);
+
+        characterController.Move(movementDirection * speed * Time.deltaTime);
+    }
+
+    /*
+    private void VerticalMovement()
+    {
+        bool isGrounded = characterController.isGrounded;
+
+        if (verticalVelocity.y < 0f)
+        {
+            verticalVelocity.y = -2f;
+        }
+
+        if (isGrounded && jumpRequested)
+        {
+            verticalVelocity.y = jumpForce;
+            jumpRequested = false;
+        }
+
+        verticalVelocity.y += gravity * Time.deltaTime;
+        characterController.Move(verticalVelocity * Time.deltaTime);
+    }
+    */
 }
