9e2f92dc38362f258d935dfdda12b14f41089aa9
Rerispingo <bernardo.o.horta@gmail.com>
2026-08-11 08:36:32 -0300

    feat: add player cursor control

---
+++ Assets/Scripts/Player/PlayerCursor.cs
@@ -0,0 +1,55 @@
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCursor : MonoBehaviour
+{
+    [SerializeField] private CinemachineInputAxisController cameraInputAxisController;
+
+    private PlayerMovement playerMovement;
+    private bool isCursorLocked = true;
+
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        LockCursor();
+    }
+
+    public void AlternateCursor(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isCursorLocked = !isCursorLocked;
+
+            if (isCursorLocked)
+            {
+                UnlockCursor();
+                playerMovement.SetMovementEnabled(false);
+                SetCameraInputEnabled(false);
+            }
+            else
+            {
+                LockCursor();
+                playerMovement.SetMovementEnabled(true);
+                SetCameraInputEnabled(true);
+            }
+        }
+    }
+
+    private void SetCameraInputEnabled(bool enabled)
+    {
+        cameraInputAxisController.enabled = enabled;
+    }
+
+    private void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,25 +8,40 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 8f;
     [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float maxFallSpeed = -20f;
 
     private CharacterController characterController;
+    private Camera mainCamera;
     private Vector3 verticalVelocity;
     private Vector2 moveInput;
     private bool jumpRequested;
     private float groundCheckDistance = 0.1f;
-    private float maxFallSpeed = -20f;
+    private bool isMovementEnabled = true;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        HorizontalMovement();
+        if (isMovementEnabled)
+        {
+            FollowCameraRotation();
+            HorizontalMovement();
+        }
         VerticalMovement();
+    }
+
+    // --------------- Public Methods ---------------
+
+    public void SetMovementEnabled(bool enabled)
+    {
+        isMovementEnabled = enabled;
     }
 
     // --------------- Input Actions ---------------
@@ -48,10 +63,23 @@ public class PlayerMovement : MonoBehaviour
 
     private void HorizontalMovement()
     {
-        Vector3 movementDirection = (transform.right * moveInput.x) + (transform.forward * moveInput.y);
+        Vector3 cameraRight = mainCamera.transform.right;
+        Vector3 cameraForward = mainCamera.transform.forward;
+        cameraRight.y = 0f;
+        cameraForward.y = 0f;
+        cameraRight.Normalize();
+        cameraForward.Normalize();
+
+        Vector3 movementDirection = (cameraRight * moveInput.x) + (cameraForward * moveInput.y);
         movementDirection = Vector3.ClampMagnitude(movementDirection, 1f);
 
         characterController.Move(movementDirection * speed * Time.deltaTime);
+    }
+
+    private void FollowCameraRotation()
+    {
+        float cameraYaw = mainCamera.transform.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(0f, cameraYaw, 0f);
     }
 
     private void VerticalMovement()
