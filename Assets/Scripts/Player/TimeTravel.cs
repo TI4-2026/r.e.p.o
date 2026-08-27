62bc3e381e4697dc4e320618b5bb96105c47de22
ArthurDof <arthurdoffemond@gmail.com>
2026-08-27 12:17:28 -0300

    viajem no tempo
    mecanica de viajar no tempo adicionada

---
+++ Assets/Scripts/Player/TimeTravel.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class TimeTravel : MonoBehaviour
+{
+    public GameObject past;
+    public GameObject future;
+    public Slider cooldownSlider;
+    float cooldown;
+    void Start()
+    {
+        cooldownSlider.maxValue = 2f;
+        cooldownSlider.minValue = 0f;
+        cooldown = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownSlider.value = cooldown;
+        if (cooldown < 2f)
+        {
+            cooldown += Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.E) && cooldown >= 2f)
+        {
+            StartCoroutine(ChangeTime(0.5f)); 
+            cooldown = 0f;
+        }
+    }
+    private IEnumerator ChangeTime(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        future.SetActive(!future.activeSelf);
+        past.SetActive(!past.activeSelf);
+    }
+}
