17b5aa99983e04c84e4bc5f684a74809a055859a
AlbertHugo <124901735+AlbertHugo@users.noreply.github.com>
2026-09-02 15:14:50 -0300

    Código do puzzle da caixa completo

--- Assets/Scripts/Puzzles/MoveSpawn.cs
+++ Assets/Scripts/Puzzles/MoveSpawn.cs
@@ -3,14 +3,26 @@ using UnityEngine;
 public class MoveSpawn : MonoBehaviour
 {
     private ParentPairs parentPair;
+    private Vector3 lastPos;
+    public bool isMoving;
     void Start()
     {
         parentPair = GetComponentInParent<ParentPairs>();
+        lastPos = transform.position;
     }
+    
 
-    void OnCollisionEnter(Collision collision)
+    void Update()
     {
-        parentPair.ChangePos(gameObject.transform);
-        Debug.Log("Oi");
+        if(Vector3.Distance(transform.position, lastPos) >= 0.001f)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        if (isMoving)
+            parentPair.ChangePos(transform);
     }
 }
